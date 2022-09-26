a0033f3337fe4c419f6a700739dd23af0c760f5c
Akash Maurya <akashamaurya@gmail.com>
2022-09-27 01:18:38 +0530

    Chapter_3

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -5,54 +5,91 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
+    private Rigidbody2D body;
+    // public Animation anime;
+    // bool keyPressed;
+    public float speed;
+    public float jumpForce = 2.0f;
+    public bool isGrounded;
 
     private void Awake(){
         Debug.Log("Palyer controller awake");
+        body = gameObject.GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+     
     }
 
-    // private void OnCollisionEnter2D(Collision2D collision){
-    //     Debug.Log("Collision: " + collision.gameObject.name);
-    // }
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-        
-    // }
-
-    // // Update is called once per frame
+    void OnCollisionEnter2D(){
+        isGrounded = true;
+    }
     private void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(speed));
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float jump = Input.GetAxisRaw("Jump");
 
-        float jump = Input.GetAxisRaw("Vertical");
+        Movecharacter(horizontal);
+        PlayMovementAnimation(horizontal);
+        Crouch();
+        JumpAnimation(jump);
+    }
+
+    private void JumpAnimation(float jump){
+
+        if(jump > 0){
+            animator.SetBool("Jump",true);   
+            body.AddForce(new Vector2(0f, jumpForce),ForceMode2D.Force);
+            isGrounded = false;
+        }
+        else
+            animator.SetBool("Jump",false); 
+
+    }
+
+    private void Movecharacter(float horizontal){
+        Vector3 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        transform.position = position;
+    }
+
+    private void PlayerJumpN_Crouch(float jump){
         if(jump > 0){
             animator.SetTrigger("Jump");
-            // animator.SetBool("Jump", false);
         } 
         else if(jump < 0){
             animator.SetBool("Jump", false);
-        }else{
+        }
+        else{
             if(Input.GetKey(KeyCode.LeftControl)){
-            animator.SetBool("Crouch", true);
-            }else{
+                animator.SetBool("Crouch", true);
+            }
+            else{
                 animator.SetBool("Crouch", false);
             }
         }
-        
-        
-
- 
-
-        Vector3 scale = transform.localScale;
-        if(speed < 0){
-            scale.x = -1f * Mathf.Abs(scale.x);
-        }
-        else if(speed>0){
-            scale.x=Mathf.Abs(scale.x);
-        }
-        transform.localScale = scale;
     }
 
-   
+    private void PlayMovementAnimation(float horizontal)
+    {
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+        Vector3 scale = transform.localScale;
+        if(horizontal < 0){
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if(horizontal>0){
+            scale.x=Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;        
+    }
+
+    private void Crouch(){
+      
+        if(Input.GetKey(KeyCode.LeftControl)){
+            animator.SetBool("Crouch", true);
+        }
+        else{
+            animator.SetBool("Crouch", false);
+        }
+    }
 }
